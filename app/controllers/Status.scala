0a1c26958fe3d981fa71ea7d843c510e30991056
Stefan <stefanmeiwald@yahoo.de>
2013-11-06 17:30:29 +0100

    Added status test if user has changed his login credentials

--- app/controllers/Status.scala
+++ app/controllers/Status.scala
@@ -5,17 +5,37 @@ import helper.AuthenticatedAction
 import play.api.mvc.Action
 import scala.concurrent.Future
 import views._
+import play.api.Play
 
 object Status extends Controller {
+  implicit val context = scala.concurrent.ExecutionContext.Implicits.global
 
   def index() = {
     AuthenticatedAction {
       Action.async {
         implicit request =>
         {
-          Future.successful(Ok(html.status.status()))
+          val tests: Future[List[Boolean]] = for {
+            pwChanged <- testCredentialsChanged
+          } yield {
+            List(pwChanged)
+          }
+
+          tests map {
+            result => Ok(html.status.status(result, 75, 25))
+          } recover {
+            case _ => Ok("")
+          }
         }
       }
     }
+  }
+
+  def testCredentialsChanged: Future[Boolean] = {
+    if (Play.current.configuration.getString("fillable.user").getOrElse("").equals("admin") &&
+      Play.current.configuration.getString("fillable.password").getOrElse("").equals("pass123"))
+      Future.successful(false)
+    else
+      Future.successful(true)
   }
 }
