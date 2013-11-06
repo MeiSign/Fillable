cace87fb187f0c80d6b5c96dfb023f2e954c8794
Stefan <stefanmeiwald@yahoo.de>
2013-11-06 18:04:23 +0100

    Added git elasticsearch test

--- app/controllers/Status.scala
+++ app/controllers/Status.scala
@@ -6,6 +6,11 @@ import play.api.mvc.Action
 import scala.concurrent.Future
 import views._
 import play.api.Play
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import esclient.{EsClient, EsQuery}
+import play.api.libs.json.Json
+import play.api.i18n.Messages
+import esclient.queries.GetEsVersionQuery
 
 object Status extends Controller {
   implicit val context = scala.concurrent.ExecutionContext.Implicits.global
@@ -17,12 +22,13 @@ object Status extends Controller {
         {
           val tests: Future[List[Boolean]] = for {
             pwChanged <- testCredentialsChanged
+            version <- testElasticsearchVersion
           } yield {
-            List(pwChanged)
+            List(pwChanged, version)
           }
 
           tests map {
-            result => Ok(html.status.status(result, 75, 25))
+            result => Ok(html.status.status(result, 4, 1))
           } recover {
             case _ => Ok("")
           }
@@ -37,5 +43,29 @@ object Status extends Controller {
       Future.successful(false)
     else
       Future.successful(true)
+  }
+
+  def testElasticsearchVersion: Future[Boolean] = {
+    EsClient.execute(new GetEsVersionQuery) map {
+      esVersion => {
+        val version: Option[String] = (esVersion.json \ "version" \ "number").asOpt[String]
+        val buildTimestamp: Option[String] = (esVersion.json \ "version" \ "build_timestamp").asOpt[String]
+
+        if (version.isDefined && buildTimestamp.isDefined) {
+          try {
+            isAfterOrEqualToRequiredBuildtime(buildTimestamp.get)
+          } catch {
+            case _ => false
+          }
+        } else {
+          false
+        }
+      }
+    }
+  }
+
+  def isAfterOrEqualToRequiredBuildtime(buildtime: String): Boolean = {
+    val fmt: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    (fmt.parseDateTime(buildtime).isAfter((fmt.parseDateTime("2013-09-17T12:50:20Z"))) || fmt.parseDateTime(buildtime).isEqual((fmt.parseDateTime("2013-09-17T12:50:20Z"))))
   }
 }
---
+++ app/esclient/queries/GetEsVersionQuery.scala
@@ -0,0 +1,19 @@
+package esclient.queries
+
+import play.api.libs.json.JsObject
+import esclient.EsQuery
+import esclient.HttpType
+import play.api.libs.json.Json
+import play.api.libs.json.JsValue
+import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.DateTimeFormat
+import play.api.i18n.Messages
+import play.api.libs.ws.Response
+
+class GetEsVersionQuery extends EsQuery {
+ val httpType = HttpType.Get
+  
+  def getUrlAddon: String = "/"
+  
+  def toJson: JsObject = Json.obj()
+}
