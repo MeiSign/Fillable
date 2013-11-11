bfbb2321bc2039255b1822578785566f55039f7c
Stefan <stefanmeiwald@yahoo.de>
2013-11-11 14:10:38 +0100

    Added Optionboxgeneration to js snippet

--- public/javascripts/fillable.js
+++ public/javascripts/fillable.js
@@ -1,19 +1,42 @@
-var FillableWidget = {
+var settings, FillableWidget = {
     settings: {
     },
 
     init: function(inputFieldId) {
-        this.settings.inputField = document.getElementById(inputFieldId);
-        this.settings.form = this.getFormFrom();
+        settings = this.settings;
+        settings.inputField = document.getElementById(inputFieldId);
+        settings.form = this.getFormFrom();
         this.bindUiActions();
     },
 
     bindUiActions: function() {
-        this.settings.form.setAttribute("onsubmit", "FillableWidget.submitForm()")
+        settings.form.setAttribute("onsubmit", "FillableWidget.submitForm()");
+        this.createOptionBox();
     },
 
     getFormFrom: function() {
-        return this.settings.inputField.form;
+        return settings.inputField.form;
+    },
+
+    createOptionBox: function() {
+        var styleOptionbox = function(optionBox) {
+            optionBox.setAttribute("class", "flbOptionbox");
+            var rect = settings.inputField.getBoundingClientRect();
+            optionBox.style.left = rect.left + "px";
+            optionBox.style.top = rect.bottom + "px";
+            optionBox.style.minWidth = settings.inputField.offsetWidth + "px"
+        }
+
+        var optionBox = document.createElement("ul");
+        optionBox.innerHTML = "<li class='flbOption' onClick='FillableWidget.selectOption(this.innerText)'>test0</li><li class='flbOption' onClick='FillableWidget.selectOption(this.innerText)'>test1</li><li class='flbOption' onClick='FillableWidget.selectOption(this.innerText)'>test2</li>";
+        styleOptionbox(optionBox);
+        settings.inputField.parentNode.insertBefore(optionBox, settings.inputField)
+    },
+
+    selectOption: function(option) {
+        settings.chosenOption = option;
+        settings.inputField.value = option;
+        settings.inputField.focus();
     },
 
     submitForm: function() {
