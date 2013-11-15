76bbcef235763d5bb854694c50dd2396435b00b2
Stefan <stefanmeiwald@yahoo.de>
2013-11-15 13:44:24 +0100

    Added Ajax Call to js snippet

--- public/javascripts/fillable.js
+++ public/javascripts/fillable.js
@@ -3,22 +3,26 @@ var settings, FillableWidget = {
         values: {}
     },
 
-    init: function(inputFieldId) {
+    init: function(inputFieldId, fblHost, fblIndex) {
         settings = this.settings;
         settings.inputField = document.getElementById(inputFieldId);
-        settings.inputField.setAttribute("autocomplete", "off");
         settings.form = this.getFormFrom();
+        settings.host = fblHost;
+        settings.index = fblIndex;
         this.createUi();
         this.bindUiActions();
     },
 
     createUi: function() {
         this.createOptionBox();
+        settings.inputField.setAttribute("autocomplete", "off");
     },
 
     bindUiActions: function() {
         settings.form.setAttribute("onsubmit", "FillableWidget.submitForm()");
-        settings.inputField.onkeyup = function() { FillableWidget.changeInput(); }
+        settings.inputField.onkeyup = function() {
+            FillableWidget.changeInput();
+        }
 
         document.querySelector('body').addEventListener('click', function(event) {
             if (event.target.className === 'fblOption') {
@@ -33,7 +37,7 @@ var settings, FillableWidget = {
 
     createOptionBox: function() {
         var styleOptionbox = function(optionBox) {
-            optionBox.setAttribute("class", "fblOptionbox");
+            optionBox.setAttribute("class", "fblOptionbox fblHidden");
             var rect = settings.inputField.getBoundingClientRect();
             optionBox.style.left = rect.left + "px";
             optionBox.style.top = rect.bottom + "px";
@@ -41,7 +45,6 @@ var settings, FillableWidget = {
         }
 
         var optionBox = document.createElement("ul");
-        optionBox.innerHTML = "<li class='fblOption'>test0 test0 test0 </li><li class='fblOption'>test1</li><li class='fblOption'>test2</li>";
         styleOptionbox(optionBox);
         settings.inputField.parentNode.insertBefore(optionBox, settings.inputField)
         settings.optionBox = optionBox;
@@ -58,12 +61,50 @@ var settings, FillableWidget = {
         if (settings.values.typed != settings.inputField.value) {
             settings.values.typed = settings.inputField.value;
             FillableHelper.removeClass(settings.optionBox, "fblHidden");
-            console.log(settings.values.typed)
+            this.getOptions();
         }
     },
 
     submitForm: function() {
         alert("Select")
+    },
+
+    getOptions: function() {
+        var http = getHttpRequestObject();
+        if (http != null) {
+            http.open("GET", getOptionsUrl(), true);
+            http.send();
+            http.onreadystatechange = function() {
+                if ((http.readyState == 4) && (http.status == 200)) {
+                    emptyOptionList();
+                    var json = eval("("+http.responseText+")")
+                    for (var i = 0; i < json.options.length; i++) {
+                        var newLi = document.createElement("li");
+                        var text = document.createTextNode(json.options[i].text);
+                        newLi.appendChild(text);
+                        settings.optionBox.appendChild(newLi)
+                    }
+                }
+            }
+        }
+
+        function getHttpRequestObject() {
+            if (window.XMLHttpRequest) {
+                return new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                return new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        }
+
+        function getOptionsUrl() {
+            return settings.host + "/complete/" + settings.index + "/" + settings.inputField.value;
+        }
+
+        function emptyOptionList() {
+            while (settings.optionBox.firstChild) {
+                settings.optionBox.removeChild(settings.optionBox.firstChild);
+            }
+        }
     }
 }
 
