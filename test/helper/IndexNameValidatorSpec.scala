e1633f5c4f232688b71acc4cdb7a935ac74e9ca5
Stefan <stefanmeiwald@yahoo.de>
2013-11-18 14:49:01 +0100

    Added tests for optionDocument model and changed test of indexNameValidator

--- test/helper/IndexNameValidatorSpec.scala
+++ test/helper/IndexNameValidatorSpec.scala
@@ -7,7 +7,7 @@ class IndexNameValidatorSpec extends Specification {
 
   "IndexNameValidatorSpec" should {
     "containsOnlyValidChars return true for valid index names" in new WithApplication{
-      IndexNameValidator.containsOnlyValidChars("SDGSDF12_bcr0") must beTrue
+      IndexNameValidator.containsOnlyValidChars("dsfgdsfgsd12_bcr0") must beTrue
     }
 
     "containsOnlyValidChars return false for invalid index names" in new WithApplication{
