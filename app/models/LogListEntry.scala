790958addc9eaf7ffac300225249e24ebffcb5fd
Stefan <stefanmeiwald@yahoo.de>
2013-12-10 11:36:47 +0100

    Added method to LogListEntries to retrieve index name of logfile

--- app/models/LogListEntry.scala
+++ app/models/LogListEntry.scala
@@ -1,3 +1,5 @@
 package models
 
-case class LogListEntry(name: String, logEntries: Long, size: Long)
+case class LogListEntry(name: String, logEntries: Long, size: Long) {
+  def indexName = name.replace("_log", "")
+}
